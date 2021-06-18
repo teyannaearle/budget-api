72f575b399348740825e84fb8316c58a306ec1b0
Teyanna Earle <teyonna.earle92@gmail.com>
2021-06-18 14:16:47 -0400

    test id

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,89 +1,88 @@
-const express = require("express")
-var moment = require('moment');
+const express = require("express");
+var moment = require("moment");
 
-var uuid = require('uuid')
+var uuid = require("uuid");
 
-const transactions = express.Router()
-const transactionsArray = require ("../models/transaction")
-
+const transactions = express.Router();
+const transactionsArray = require("../models/transaction");
 
 const validateBody = (req, res, next) => {
-    const { name, from, date, amount} = req.body
-    
-    let result = moment(date, 'YYYY-MM-DD',true).isValid();
+  const { name, from, date, amount } = req.body;
 
-    if (!name || !from || !date || !amount ) {
-        res.status(400).send()
-    } else if (result){
-        next()
-    } else {
-        res.status(400).send()
-    }
+  let result = moment(date, "YYYY-MM-DD", true).isValid();
 
-}
+  if (!name || !from || !date || !amount) {
+    res.status(400).send();
+  } else if (result) {
+    next();
+  } else {
+    res.status(400).send();
+  }
+};
 
-transactions.get("/" , (req, res) => {
-    res.status(200).json(transactionsArray)
-})
+transactions.get("/", (req, res) => {
+  res.status(200).json(transactionsArray);
+});
 
-transactions.get("/:id", (req,res) => {
-    const { id } = req.params
-    const matchesID = (transaction) => transaction.id === Number(id)
-    const idx = transactionsArray.findIndex(matchesID)
+transactions.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const matchesID = (transaction) => transaction.id === Number(id);
+  const idx = transactionsArray.findIndex(matchesID);
 
-    if (idx >= 0){
-        res.status(200).json(transactionsArray[idx])
-    } else {
-        res.redirect("/404")
-    }
-
-})
+  if (idx >= 0) {
+    res.status(200).json(transactionsArray[idx]);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 transactions.post("/", validateBody, (req, res) => {
-    // if (!req.body.id){
-    //     let id = transactionsArray[transactionsArray.length -1].id +1
-    //     transactionsArray.push({id: id, ...req.body})
-    // } else {
-    //     transactionsArray.push(req.body)
-    // }
-    if (transactionsArray[0]){
-       let id = transactionsArray[transactionsArray.length -1].id +1
-       transactionsArray.push({id: id, ...req.body})
+  if (!req.body.id) {
+    if (transactionsArray[0]) {
+      let id = transactionsArray[transactionsArray.length - 1].id + 1;
+      transactionsArray.push({ id: id, ...req.body });
     } else {
-        let id = 0
-        transactionsArray.push({id: id, ...req.body})
+      transactionsArray.push({ id: 0, ...req.body });
     }
+  } else {
+    transactionsArray.push(req.body);
+  }
+  // if (transactionsArray[0]){
+  //    let id = transactionsArray[transactionsArray.length -1].id +1
+  //    transactionsArray.push({id: id, ...req.body})
+  // } else {
+  //     let id = 0
+  //     transactionsArray.push({id: id, ...req.body})
+  // }
 
-    
-    res.json(transactionsArray[transactionsArray.length -1])
-})
+  res.json(transactionsArray[transactionsArray.length - 1]);
+});
 
 transactions.delete("/:id", (req, res) => {
-    const { id } = req.params
-    const matchesID = (transaction) => transaction.id === Number(id)
-    const idx = transactionsArray.findIndex(matchesID)
+  const { id } = req.params;
+  const matchesID = (transaction) => transaction.id === Number(id);
+  const idx = transactionsArray.findIndex(matchesID);
 
-    if (idx >= 0){
-        transactionsArray.splice(idx,1)
-        res.status(200).json(transactionsArray)
-    } else {
-        res.redirect("/404")
-    }
-})
+  if (idx >= 0) {
+    transactionsArray.splice(idx, 1);
+    res.status(200).json(transactionsArray);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 transactions.put("/:id", (req, res) => {
-    const { id } = req.params
-    const matchesID = (transaction) => transaction.id === Number(id)
-    const idx = transactionsArray.findIndex(matchesID)
+  const { id } = req.params;
+  const matchesID = (transaction) => transaction.id === Number(id);
+  const idx = transactionsArray.findIndex(matchesID);
 
-    if (idx >=0){
-        const transactionID = transactionsArray[idx].id
-        transactionsArray[idx] = {...req.body, id: transactionID}
-        res.status(200).json(transactionsArray)
-    } else {
-        res.redirect("/404")
-    }
+  if (idx >= 0) {
+    const transactionID = transactionsArray[idx].id;
+    transactionsArray[idx] = { ...req.body, id: transactionID };
+    res.status(200).json(transactionsArray);
+  } else {
+    res.redirect("/404");
+  }
+});
 
-})
-
-module.exports = transactions
+module.exports = transactions;
